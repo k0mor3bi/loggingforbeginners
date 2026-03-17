2437b323049b83671b27c689df513f16ecfd89a1
Elea <zenoen@dukes.jmu.edu>
2026-03-17 18:58:36 -0400

    Add files via upload
    Here's a practice to start with and try out on your own! 
    (Yes, I like One Piece.)

---
+++ mustknows.py
@@ -0,0 +1,19 @@
+print('Hello, Strawhats!')
+    #with this, we can print a message to the console. We can also use variables to store values and then print them.
+
+name = 'Sanji'
+print('Hello,' + name + '!')
+    #using ' with + helps us to concatenate strings and variables together. We can also use f-strings to make it easier to read.
+    #f-strings are a way to format strings in Python. They allow us to embed expressions inside string literals, using curly braces {}.
+
+#Let's practice with different data types:
+text = 'Luffy'
+number = 10
+decimal = -12.5
+has_no_money = True #booleans can only be true or false
+coordinates = (2.5, 1.5)
+names = ['Sanji', 'Luffy', 'Zoro'] #list
+unique = {1, 2, 3, 4, 4, 5} #there cannot be repeat numbers, that's why its unique
+users = {'Sanji':1, 'Luffy':2, 'Zoro':3}
+#Dictornary is a way to describe and organize data. It consists of key-value pairs, where each key is unique and maps to a value.
+#In this example, we have a dictionary called 'users' where the keys are the names of the characters and the values are their corresponding numbers.
