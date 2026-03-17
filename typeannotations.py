4ae842604f5c1fd8986cce50083cb647eb1f9496
Elea <zenoen@dukes.jmu.edu>
2026-03-17 19:33:52 -0400

    Add files via upload
    Next Steps of Learning Python.

---
+++ typeannotations.py
@@ -0,0 +1,20 @@
+#type annotations
+age: int = 21
+#int specifies the type
+name: str = 'Sanji'
+#str specifies that the variable 'name' is a string
+is_pirate: bool = True
+#bool specifies that the variable 'is_pirate' is a boolean value (True or False)
+coordinates: tuple = (2.5, 1.5)
+#tuple specifies that the variable 'coordinates' is a tuple, which is an ordered collection of elements that can be of different types
+names: list = ['Sanji', 'Luffy', 'Zoro']
+#list specifies that the variable 'names' is a list, which is an ordered collection of elements that can be of different types
+unique_numbers: set = {1, 2, 3, 4, 5}
+#set specifies that the variable 'unique_numbers' is a set, which is an unordered collection of unique elements
+users: dict = {'Sanji': 1, 'Luffy': 2, 'Zoro': 3}
+#dict specifies that the variable 'users' is a dictionary, which is a collection of key-value pairs
+
+#practice for you
+#run your python and get the following answers: (Name, Age, Is a Pirate?, Coordinates, Names, Unique Numbers, Users)
+#what are the type annotations for each variable? Try to guess before running the code!
+#what code should you say to print the type of each variable? (Hint: use the type() function)
